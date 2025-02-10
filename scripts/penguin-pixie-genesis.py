66131ebd6a84ab3c40ebcc369fcb65e1baaf01e9
diewland <diewland@gmail.com>
2025-02-10 20:39:42 +0700

    add json

---
+++ scripts/penguin-pixie-genesis.py
@@ -0,0 +1,34 @@
+from pprint import pprint as pp
+import json
+
+NAME = "Penguin Pixie Genesis"
+DESC = "A tiny girl with blue eyes and her penguin pals, waking up to the soft morning glow in the polar frost."
+FROM_ID = 1
+SUPPLY = 888
+IMG_IPFS = "https://diewcoftza.github.io/ipfs/penguin-pixie-genesis/asset.png"
+VID_IPFS = "https://diewcoftza.github.io/ipfs/penguin-pixie-genesis/asset.mp4"
+OUTPUT_PATH = "../docs/penguin-pixie-genesis/json/{}"
+
+for i in range(0, SUPPLY):
+    token_id = i + FROM_ID
+    dest = OUTPUT_PATH.format(token_id)
+
+    # craft data
+    data = {
+        "name": "{} #{}".format(NAME, token_id),
+        "description": DESC,
+        "image": IMG_IPFS,
+        "animation_url": VID_IPFS,
+        #"attributes": [
+        #    {
+        #        "trait_type": "Rarity",
+        #        "value": "Common",
+        #    },
+        #],
+    }
+
+    # write file
+    print(dest)
+    #pp(data)
+    with open(dest, "w") as f:
+        json.dump(data, f)
